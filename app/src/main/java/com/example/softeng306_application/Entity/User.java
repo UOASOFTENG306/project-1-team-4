c260218e1a78b70cca73a748859fc9d4cef08573
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-12 13:19:34 +1200

    chore: add initial User class

---
+++ app/src/main/java/com/example/softeng306_application/Entity/User.java
@@ -0,0 +1,28 @@
+package com.example.softeng306_application.Entity;
+
+public class User {
+    private String userID;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
