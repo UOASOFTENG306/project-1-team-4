34e42a06be89f4fb6463fdc2ba8b3d86417d0dc9
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2023-08-13 19:49:45 +1200

    chore: add initial UseCases into UseCase package

---
+++ app/src/main/java/com/example/softeng306_application/UseCase/GetCategoriesUseCase.java
@@ -0,0 +1,10 @@
+package com.example.softeng306_application.UseCase;
+
+import com.example.softeng306_application.Entity.Category;
+
+import java.util.List;
+
+public class GetCategoriesUseCase {
+
+    public List<Category> getCategories() { return null; }
+}
---
+++ app/src/main/java/com/example/softeng306_application/UseCase/GetRestaurantUseCase.java
@@ -0,0 +1,8 @@
+package com.example.softeng306_application.UseCase;
+
+import com.example.softeng306_application.Entity.Restaurant;
+
+public class GetRestaurantUseCase {
+
+    public Restaurant getRestaurant(String restaurantID) { return null; }
+}
