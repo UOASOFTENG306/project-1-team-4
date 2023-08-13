b28d2ae0d6e13cb47a3bd0f2e0909058285cb276
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2023-08-13 19:00:44 +1200

    chore: add ListActivity view

---
+++ app/src/main/java/com/example/softeng306_application/View/ListActivity.java
@@ -0,0 +1,16 @@
+package com.example.softeng306_application.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.softeng306_application.R;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
