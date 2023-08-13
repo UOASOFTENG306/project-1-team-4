11b295637d4f26dea8f18b634fa61f713baea2c6
nroh555 <nroh555@aucklanduni.ac.nz>
2023-08-13 20:48:25 +1200

    Chore/Setup-Category-Repository

--- app/src/main/java/com/example/softeng306_application/Repository/ICategoryRepository.java
+++ app/src/main/java/com/example/softeng306_application/Repository/ICategoryRepository.java
@@ -1,4 +1,11 @@
 package com.example.softeng306_application.Repository;
 
+import com.example.softeng306_application.Entity.Category;
+
+import java.util.List;
+
 public interface ICategoryRepository {
+    CategoryRepository getInstance();
+
+    List<Category> getCategories();
 }
