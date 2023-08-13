084ee87ea99e16ca0e3ea645617a923ac1b5a788
nroh555 <nroh555@aucklanduni.ac.nz>
2023-08-13 20:46:01 +1200

    Chore/Setup-Restaurant-Repository

--- app/src/main/java/com/example/softeng306_application/Repository/IRestaurantRepository.java
+++ app/src/main/java/com/example/softeng306_application/Repository/IRestaurantRepository.java
@@ -1,4 +1,16 @@
 package com.example.softeng306_application.Repository;
 
+import com.example.softeng306_application.Entity.Restaurant;
+
+import java.util.List;
+
 public interface IRestaurantRepository {
+    RestaurantRepository getInstance();
+    List<Restaurant> getTopRatedRestaurants();
+
+    List<Restaurant> getRestaurants();
+
+    Restaurant getRestaurant(String restaurantID);
+
+    List <Restaurant> searchRestaurants(String term);
 }
