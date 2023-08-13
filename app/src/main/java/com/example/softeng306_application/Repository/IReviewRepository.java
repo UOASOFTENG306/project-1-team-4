197d488f5831ba67d7d1491f773291ca8fc77bf9
nroh555 <nroh555@aucklanduni.ac.nz>
2023-08-13 20:53:31 +1200

    chore: setup review repository

--- app/src/main/java/com/example/softeng306_application/Repository/IReviewRepository.java
+++ app/src/main/java/com/example/softeng306_application/Repository/IReviewRepository.java
@@ -1,4 +1,15 @@
 package com.example.softeng306_application.Repository;
 
+import com.example.softeng306_application.Entity.Review;
+
+import java.util.List;
+
 public interface IReviewRepository {
+    ReviewRepository getInstance();
+
+    List<Review> getReviews(String restaurantID);
+
+    void addReview(String restaurantID, String userID);
+
+    void deleteReview(String restaurantID);
 }
--- app/src/main/java/com/example/softeng306_application/Repository/ReviewRepository.java
+++ app/src/main/java/com/example/softeng306_application/Repository/ReviewRepository.java
@@ -1,4 +1,27 @@
 package com.example.softeng306_application.Repository;
 
+import com.example.softeng306_application.Entity.Review;
+
+import java.util.List;
+
 public class ReviewRepository implements IReviewRepository {
+    @Override
+    public ReviewRepository getInstance() {
+        return null;
+    }
+
+    @Override
+    public List<Review> getReviews(String restaurantID) {
+        return null;
+    }
+
+    @Override
+    public void addReview(String restaurantID, String userID) {
+
+    }
+
+    @Override
+    public void deleteReview(String restaurantID) {
+
+    }
 }
