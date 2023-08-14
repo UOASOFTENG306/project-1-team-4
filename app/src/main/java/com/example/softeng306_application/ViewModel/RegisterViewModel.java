d4379f89aea9a02ec12e30ae2cc6a58f515b9a15
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-14 21:52:46 +1200

    feat: couple register viewmodel with repository

--- app/src/main/java/com/example/softeng306_application/ViewModel/RegisterViewModel.java
+++ app/src/main/java/com/example/softeng306_application/ViewModel/RegisterViewModel.java
@@ -5,8 +5,20 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.softeng306_application.Entity.User;
+import com.example.softeng306_application.Repository.UserRepository;
+
 public class RegisterViewModel extends AndroidViewModel {
+
+    private UserRepository userRepository;
+    private User user;
     public RegisterViewModel(@NonNull Application application) {
         super(application);
+        userRepository = userRepository.getInstance();
+
+    }
+
+    public void register(String email, String password){
+        userRepository.register(email, password);
     }
 }
