85679160261e63294bb11905036dd271daabadb9
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-14 22:47:03 +1200

    feat: add views on login activity

--- app/src/main/java/com/example/softeng306_application/View/LoginActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/LoginActivity.java
@@ -1,19 +1,34 @@
 package com.example.softeng306_application.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import com.example.softeng306_application.R;
+import com.example.softeng306_application.ViewModel.LoginViewModel;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class LoginActivity extends AppCompatActivity implements Activity  {
-
+    TextInputEditText editTextEmail, editTextPassword;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+            }
+        });
     }
 
     public void showRegisterActivity(View v) {
