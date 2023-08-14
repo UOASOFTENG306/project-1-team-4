89295f823b6e06c1706cf68dc01f5c65366c7e17
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-14 22:43:43 +1200

    feat: couple login viewmodel to user repository

--- app/src/main/java/com/example/softeng306_application/ViewModel/LoginViewModel.java
+++ app/src/main/java/com/example/softeng306_application/ViewModel/LoginViewModel.java
@@ -1,4 +1,28 @@
 package com.example.softeng306_application.ViewModel;
 
-public class LoginViewModel implements ViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.softeng306_application.Entity.User;
+import com.example.softeng306_application.Repository.UserRepository;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+
+public class LoginViewModel extends AndroidViewModel {
+
+    private UserRepository userRepository;
+    public LoginViewModel(@NonNull Application application) {
+        super(application);
+        userRepository = userRepository.getInstance();
+    }
+
+    public Task<AuthResult> signIn(String email, String password){
+        return userRepository.signIn(email, password);
+    }
+
+    public void register(String email, String password){
+        userRepository.register(email, password);
+    }
 }
