5af885cb3e3c578c6225de4f06b8acfd766ad3e0
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2023-08-14 22:00:33 +1200

    feat: add register button

--- app/src/main/java/com/example/softeng306_application/View/LoginActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.softeng306_application.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.softeng306_application.R;
 
@@ -12,5 +14,10 @@ public class LoginActivity extends AppCompatActivity implements Activity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void showRegisterActivity(View v) {
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        startActivity(registerIntent);
     }
 }
