24b34eac4255a3e6c216872a64a63d0a828e2286
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-15 23:10:11 +1200

    feat: add ViewHolder class

--- app/src/main/java/com/example/softeng306_application/View/MainActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.softeng306_application.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.softeng306_application.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private class ViewHolder{
+        Button logoutButton;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
