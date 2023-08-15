99c261b2b0cdeef728e3c66a4fbf25b389ec3b66
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-15 23:21:00 +1200

    feat: couple main view to viewmodel

--- app/src/main/java/com/example/softeng306_application/View/MainActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.softeng306_application.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.example.softeng306_application.R;
+import com.example.softeng306_application.Repository.UserRepository;
+import com.example.softeng306_application.ViewModel.LoginViewModel;
+import com.example.softeng306_application.ViewModel.MainViewModel;
+import com.example.softeng306_application.ViewModel.RegisterViewModel;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MainViewModel mainViewModel;
     private class ViewHolder{
         Button logoutButton;
     }
@@ -17,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
     }
 }
