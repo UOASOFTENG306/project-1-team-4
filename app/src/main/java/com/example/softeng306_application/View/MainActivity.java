c24c2121e03982d572fbd8a128d5ac477858c08d
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-17 00:25:20 +1200

    refactor: change recyclerview code to suit MVVM

--- app/src/main/java/com/example/softeng306_application/View/MainActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.softeng306_application.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -23,10 +25,13 @@ import org.checkerframework.checker.units.qual.A;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private MainViewModel mainViewModel;
+    private CategoryRecyclerAdapter categoryRecyclerAdapter;
+    private TopRatedRecylerAdapter topRatedAdapter;
     private class ViewHolder{
         TextView usernameText;
         Button logoutButton;
@@ -47,46 +52,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // TODO: THIS IS FOR TESTING PURPOSES!!!
         // Binding TopRatedRecyclerAdapter
         vh.topRatedRecyclerView = findViewById(R.id.recview_top_rated);
 
-        // Populate topRatedList
-        ArrayList<Restaurant> topRatedList = new ArrayList<Restaurant>();
-        Restaurant res1 = new Restaurant();
-        res1.setLogoImage("res1");
-        Restaurant res2 = new Restaurant();
-        res2.setLogoImage("res2");
-        Restaurant res3 = new Restaurant();
-        res3.setLogoImage("res3");
-
-        topRatedList.add(res1);
-        topRatedList.add(res2);
-        topRatedList.add(res3);
-
-        // TODO: THIS IS FOR TESTING PURPOSES!!!
         // Binding CategoryRecyclerAdapter
         vh.categoryRecyclerView = findViewById(R.id.recview_categories);
 
-        // Populate categoryList
-        ArrayList<Category> categoryList = new ArrayList<Category>();
-        Category cat1 = new Category();
-        cat1.setCategoryType(CategoryType.CAFE);
-        Category cat2 = new Category();
-        cat2.setCategoryType(CategoryType.ASIAN);
-        Category cat3 = new Category();
-        cat3.setCategoryType(CategoryType.EUROPEAN);
-        Category cat4 = new Category();
-        cat4.setCategoryType(CategoryType.FAST_FOOD);
-
-        categoryList.add(cat1);
-        categoryList.add(cat2);
-        categoryList.add(cat3);
-        categoryList.add(cat4);
-
         // Create adapters passing in the test lists
-        TopRatedRecylerAdapter topRatedAdapter = new TopRatedRecylerAdapter(this, topRatedList);
-        CategoryRecyclerAdapter categoryRecyclerAdapter = new CategoryRecyclerAdapter(this, categoryList);
+        topRatedAdapter = new TopRatedRecylerAdapter(this, mainViewModel.getTopRatedRestaurants());
+        categoryRecyclerAdapter = new CategoryRecyclerAdapter(this, mainViewModel.getCategories());
 
         // Attach adapter to the recycler view to populate these items
         vh.topRatedRecyclerView.setAdapter(topRatedAdapter);
--- app/src/main/java/com/example/softeng306_application/ViewModel/MainViewModel.java
+++ app/src/main/java/com/example/softeng306_application/ViewModel/MainViewModel.java
@@ -1,13 +1,23 @@
 package com.example.softeng306_application.ViewModel;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.softeng306_application.Entity.Category;
+import com.example.softeng306_application.Entity.CategoryType;
+import com.example.softeng306_application.Entity.Restaurant;
+import com.example.softeng306_application.Repository.RestaurantRepository;
 import com.example.softeng306_application.Repository.UserRepository;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     private UserRepository userRepository;
@@ -18,6 +28,45 @@ public class MainViewModel extends AndroidViewModel {
     }
     public Task<DocumentSnapshot> getUserInfo() {
         return userRepository.getAllUserInformation();
+    }
+
+    public List<Category> getCategories(){
+        // TODO: RETRIEVE DATA FROM REPOSITORY(?)
+
+        // Populate categoryList
+        ArrayList<Category> categoryList = new ArrayList<Category>();
+        Category cat1 = new Category();
+        cat1.setCategoryType(CategoryType.CAFE);
+        Category cat2 = new Category();
+        cat2.setCategoryType(CategoryType.ASIAN);
+        Category cat3 = new Category();
+        cat3.setCategoryType(CategoryType.EUROPEAN);
+        Category cat4 = new Category();
+        cat4.setCategoryType(CategoryType.FAST_FOOD);
+
+        categoryList.add(cat1);
+        categoryList.add(cat2);
+        categoryList.add(cat3);
+        categoryList.add(cat4);
+
+        return categoryList;
+    }
+    public List<Restaurant> getTopRatedRestaurants(){
+        // TODO: RETRIEVE DATA FROM REPOSITORY
+        // Initialize contacts
+        ArrayList<Restaurant> topRatedList = new ArrayList<Restaurant>();
+        Restaurant res1 = new Restaurant();
+        res1.setLogoImage("toprated 1");
+        Restaurant res2 = new Restaurant();
+        res2.setLogoImage("toprated 2");
+        Restaurant res3 = new Restaurant();
+        res3.setLogoImage("toprated 3");
+
+        topRatedList.add(res1);
+        topRatedList.add(res2);
+        topRatedList.add(res3);
+
+        return topRatedList;
     }
 
     public void logout(){
