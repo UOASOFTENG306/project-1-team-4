a985f8a10f3fcfc59eebbee0a04b6e605b7b8ead
Kai Hirafune <khir829@aucklanduni.ac.nz>
2023-08-20 02:39:04 +1200

    chore: couple detail viewmodel to repositories

--- app/src/main/java/com/example/softeng306_application/ViewModel/DetailsViewModel.java
+++ app/src/main/java/com/example/softeng306_application/ViewModel/DetailsViewModel.java
@@ -5,8 +5,18 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 
+import com.example.softeng306_application.Repository.RestaurantRepository;
+import com.example.softeng306_application.Repository.UserRepository;
+
 public class DetailsViewModel extends AndroidViewModel {
+
+    private RestaurantRepository restaurantRepository;
+    private UserRepository userRepository;
+
     public DetailsViewModel(@NonNull Application application) {
         super(application);
+        userRepository = userRepository.getInstance();
+        restaurantRepository = restaurantRepository.getInstance();
     }
+
 }
