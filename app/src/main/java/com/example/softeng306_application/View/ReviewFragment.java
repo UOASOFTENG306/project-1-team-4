e3cf501680e66f87c7a9cfce0f32422a3c3c7619
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2023-08-21 23:27:50 +1200

    feat: add initial review recycler functionality

--- app/src/main/java/com/example/softeng306_application/View/ReviewFragment.java
+++ app/src/main/java/com/example/softeng306_application/View/ReviewFragment.java
@@ -3,64 +3,55 @@ package com.example.softeng306_application.View;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.softeng306_application.Adaptor.ReviewRecyclerAdapter;
+import com.example.softeng306_application.Entity.Review;
 import com.example.softeng306_application.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ReviewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReviewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView reviewRecyclerView;
+    private ReviewRecyclerAdapter reviewRecyclerAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private List<Review> reviewList;
 
     public ReviewFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReviewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReviewFragment newInstance(String param1, String param2) {
-        ReviewFragment fragment = new ReviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_review, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_review, container, false);
+        reviewRecyclerView = view.findViewById(R.id.recview_reviews);
+
+        initialiseData();
+
+        reviewRecyclerAdapter = new ReviewRecyclerAdapter(getContext(), reviewList);
+        reviewRecyclerView.setAdapter(reviewRecyclerAdapter);
+        reviewRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
+    }
+
+    public void initialiseData() {
+        Review rev1 = new Review("luke", "this place is great");
+        Review rev2 = new Review("kai", "this place is ok");
+        Review rev3 = new Review("naren", "this place is rank");
+
+        reviewList = new ArrayList<Review>();
+        reviewList.add(rev1);
+        reviewList.add(rev2);
+        reviewList.add(rev3);
     }
 }
