cd92e246a195b242d009f873d3237fe57523ee6c
nroh555 <nroh555@aucklanduni.ac.nz>
2023-08-27 03:42:25 +1200

    fix: Deleted user data provider

--- app/src/main/java/com/example/softeng306_application/View/MainActivity.java
+++ app/src/main/java/com/example/softeng306_application/View/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.softeng306_application.View;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,32 +9,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.SearchView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.softeng306_application.Adaptor.CategoryRecyclerAdapter;
 import com.example.softeng306_application.Adaptor.TopRatedRecylerAdapter;
-import com.example.softeng306_application.Entity.Category;
-import com.example.softeng306_application.Entity.CategoryType;
-import com.example.softeng306_application.Entity.Restaurant;
 import com.example.softeng306_application.R;
 import com.example.softeng306_application.ViewModel.MainViewModel;
 import com.example.softeng306_application.dataprovider.RestaurantFirestoreDataProvider;
-import com.example.softeng306_application.dataprovider.UserFirestoreDataProvider;
-
-import org.checkerframework.checker.units.qual.A;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
