6ea5d4fd7cf283712a4cf86ec1513f8632062152
Luke Kim <lukekim@Lukes-MacBook-Pro-3.local>
2023-08-26 12:31:52 +1200

    feat: add sound effects when clicking categories; refactor border colour

--- app/src/main/java/com/example/softeng306_application/Entity/Cafe.java
+++ app/src/main/java/com/example/softeng306_application/Entity/Cafe.java
@@ -6,8 +6,8 @@ public class Cafe extends Category {
 
     // Non-argument constructor to enable auto conversion of the Firebase documents to Asian objects
     public Cafe() {
-        this.borderColour = "#C4A484";//TODO ADD PROPER BORDER COLOUR
-        this.audioFileName = "test_audio";//TODO ADD PROPER AUDIO FILE
+        this.borderColour = "#CFAF89";
+        this.audioFileName = "cafe";
         this.categoryType =  "CAFE";
     }
     @Override
--- app/src/main/java/com/example/softeng306_application/Entity/European.java
+++ app/src/main/java/com/example/softeng306_application/Entity/European.java
@@ -8,8 +8,8 @@ public class European extends Category {
 
     // Non-argument constructor to enable auto conversion of the Firebase documents to Asian objects
     public European() {
-        this.borderColour = "#0000FF";//TODO ADD PROPER BORDER COLOUR
-        this.audioFileName = "test_audio";//TODO ADD PROPER AUDIO FILE
+        this.borderColour = "#7A8DD2";
+        this.audioFileName = "european";
         this.categoryType =  "EUROPEAN";
     }
 
